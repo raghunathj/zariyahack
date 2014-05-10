8532af60f779230275c598abf7e1a4004b52b9cc
raghunathj <jrcbangalore@gmail.com>
2014-05-10 15:47:23 +0530

    geo tag

--- js/app.js
+++ js/app.js
@@ -1,10 +1,112 @@
 $(document).ready(function(){
 
+	var geocoder = new google.maps.Geocoder();
+
 	var api = {
 		'submitReport':'',
 		'getReports':''
 	};
 
-	var base_url = 'http://localhost/hack/';
+	var base_url = 'http://localhost/hack/zariyahack/';
+
+	var debug = true;
+
+	init();
+
+	function init(){
+		getCurrentLocation();
+	}
+
+	function pushDetails(){
+		var fd = [];
+		var person = $("#person").val();
+		if(person.length > 0){
+			fb += "person"
+		}
+		var doYouKnow = $("#doyouknow").val(); //Y / N
+		var firstTimeCrime = $("#firsttimecrime").val(); //Y / N
+		var incidentList = $("#incidentList").val(); //Select Box
+		var otherIncidence = $("#otherIncidence").val();
+		var location = $("#location").val();
+		var locationLat = $("#locationLat").val();
+		var locationLng = $("#locationLng").val();
+		var incidentDate = $("#incidentDate").val();
+		var incidentTime = $("#incidentTime").val();
+		var comments = $("#comments").val();
+		var firstName = $("#firstName").val();
+		var lastName = $("#lastName").val();
+		var email = $("#email").val();
+		var number = $("#number").val();
+		sendData(fd);
+	}
+
+	//Post form submission
+	function sendData(formData){
+		var dataPush = $.ajax({
+	            type: "POST",
+	            dataType: "jsonp",
+	            data:formData,
+	            processData: false,
+	            url: api.submitReport,
+	            success: function (res) {
+	            	if(res.id.length > 0){
+	            		return res.id;
+	            	}
+	            	return 0;
+	            }
+		});
+	}
+
+	//GeoTargeting
+	$(document).on("click","#getGeo",function(e){
+		e.preventDefault();
+		var ltlng = $("#latlng").val();
+		var op = getReverseLatLng(ltlng);
+	});
+
+	function getCurrentLocation(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(function(position){
+				//Success
+				log("CURRENT POSITION==========");
+				log(position);
+				if(position.coords){
+					var crd = position.coords;
+					var latLng = crd.latitude+","+crd.longitude;
+					log(latLng);
+					$("#latlng").val(latLng);
+				}
+			},function(){
+				//Error
+				return false;
+			});
+		}
+	}
+
+
+	function getReverseLatLng(latlong){
+		if(latlong.length >0){
+			var splitLatLng = latlong.split(',',2);
+			var lat = parseFloat(splitLatLng[0]);
+			var lng = parseFloat(splitLatLng[1]);
+			var latlong = new google.maps.LatLng(lat,lng);
+			geocoder.geocode({'latLng': latlong}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					log("GEO REVERSE STA=============");
+					log(results);
+					log("GEO REVERSE END=============");
+				}else{
+					return false;
+				}
+			});
+		}
+		return false;
+	}
+
+	function log(message){
+		if(debug){
+			console.log(message);
+		}
+	}
 
 });
